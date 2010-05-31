688ed79afd288da0418a5b2b0b54aa2d8cede9d5
Vladimir Parfinenko <vladimir.parfinenko@gmail.com>
2010-05-31 22:22:10 +0700

    Простенький список пользователей

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,11 @@ class UsersController < ApplicationController
   before_filter :find_user, :only => [:edit, :update]
 
   before_filter :require_no_user, :only => [:new, :create]
-  before_filter :require_user, :only => [:show, :edit, :update]
+  before_filter :require_user, :only => [:index, :show, :edit, :update]
+
+  def index
+    @users = User.all
+  end
   
   def new
     @user = User.new
