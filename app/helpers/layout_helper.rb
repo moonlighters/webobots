208cb06dbe6be23a2a25a3991c15788ba5567af4
Vladimir Parfinenko <vladimir.parfinenko@gmail.com>
2010-06-05 19:43:46 +0700

    Показ Flash'ев

---
+++ app/helpers/layout_helper.rb
@@ -0,0 +1,21 @@
+module LayoutHelper
+  def flash_sections
+    %w(error warning notice).map { |sec| flash_section(sec) } * "\n"
+  end
+
+  def flash_section(sec)
+    text = flash[sec.to_sym]
+    div_options = { 
+      :class => "flash-messages #{sec}",
+      :id => "flash-#{sec}"
+    }
+
+    if text
+      text = content_tag(:p, text)
+    else
+      div_options.merge! :style => "display:none;"
+    end
+
+    content_tag :div, text, div_options
+  end
+end
