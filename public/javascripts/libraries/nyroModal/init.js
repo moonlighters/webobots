e83d91b4791dbd1ce1519308c0203a34761cc039
Vladimir Parfinenko <vladimir.parfinenko@gmail.com>
2010-07-11 00:59:39 +0700

    Добавил nyroModal 1.6.1

---
+++ public/javascripts/libraries/nyroModal/init.js
@@ -0,0 +1,6 @@
+$(function() {
+  $.nyroModalSettings({
+      contentError: 'Страница не может быть загружена.<br/>Повторите попытку позже.<br/><br/><a href="#" class="nyroModalClose">Закрыть</a>'
+  });
+});
+
