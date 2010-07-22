8e66a96cf9f447a206ca06b3279936f971de8166
NIA <nia.fti@mail.ru>
2010-07-22 17:06:32 +0400

    Многострочные коменты нормально обрабатываются

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,5 @@
 module CommentsHelper
+  def format_comment_text(text)
+    h(text).gsub "\n", "<br/>\n"
+  end
 end
--- spec/helpers/comments_helper_spec.rb
+++ spec/helpers/comments_helper_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 
 describe CommentsHelper do
+  include CommentsHelper
 
-  #Delete this example and add some real ones or delete this file
-  it "should be included in the object returned by #helper" do
-    included_modules = (class << helper; self; end).send :included_modules
-    included_modules.should include(CommentsHelper)
+  describe "#format_comment_text" do
+    it "should add a <br/> tag to every newline character in text" do
+      format_comment_text( "a\naaa\n\nd" ).should == "a<br/>\naaa<br/>\n<br/>\nd"
+    end
+
+    it "should not change text without newline characters" do
+      ["aaa", "aa\taa", "", "bb\r"].each do |s|
+        format_comment_text( s ).should == s
+      end
+    end
+
+    it "should do escaping" do
+      format_comment_text( "a\na<a>a\n\nd" ).should == "a<br/>\na&lt;a&gt;a<br/>\n<br/>\nd"
+    end
   end
-
 end
