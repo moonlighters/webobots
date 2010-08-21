7bf58f2ffc24f0c5adcc922ed5c8779a305bc0c2
NIA <nia.fti@mail.ru>
2010-08-21 21:23:04 +0700

    Дописал "quick tour", пока что без картинок.
    Правда, это уже больше похоже скорее на "Getting started", или вообще учебник...

--- app/controllers/tour_controller.rb
+++ app/controllers/tour_controller.rb
@@ -1,4 +1,38 @@
 class TourController < ApplicationController
   def show
+    @samples = []
+    @samples[0] = "set_speed(100)"
+    @samples[1] = <<-SAMPLE1
+set_speed(50)
+while 1
+  x = posx()
+  y = posy()
+  if x < 100
+    rotate(45)
+  end
+  if x > 900
+    rotate(-135)
+  end
+  if y < 100
+    rotate(135)
+  end
+  if y > 900
+    rotate(-45)
+  end
+end
+    SAMPLE1
+    @samples[2] = "set_speed(50)"
+    @samples[3] = <<-SAMPLE3
+while 1
+  ...
+end
+    SAMPLE3
+    @samples[4] = "x = posx()"
+    @samples[5] = <<-SAMPLE5
+if x < 100
+  ...
+end
+    SAMPLE5
+    @samples[6] = "rotate(45)"
   end
 end
