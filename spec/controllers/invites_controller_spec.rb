aafc52b2254cc82782e91544f218fe1a7130d8dc
NIA <nia.fti@mail.ru>
2010-08-25 22:33:34 +0700

    спеки на InvitesController

--- spec/controllers/invites_controller_spec.rb
+++ spec/controllers/invites_controller_spec.rb
@@ -1,9 +1,59 @@
-require 'spec_helper'
+require 'controller_spec_helper'
 
 describe InvitesController do
+  include AuthlogicSpecHelpers
+  integrate_views
 
-  #Delete these examples and add some real ones
-  it "should use InvitesController" do
-    controller.should be_an_instance_of(InvitesController)
+  before { login_as_admin }
+
+  describe "#index" do
+    before { logout }
+
+    it "should work logged as admin" do
+      login_as_admin
+
+      get 'index'
+      response.should be_success
+    end
+
+    it "should not work logged as user" do
+      login
+      
+      get 'index'
+      response.should_not be_success
+    end
+
+    it "should not work not logged" do
+      get 'index'
+      response.should_not be_success
+    end
+  end
+
+  describe "#create" do
+    it "should create good ones" do
+      any_instance_of(Invite, :valid? => true)
+
+      post 'create'
+      response.should be_redirect
+      flash[:notice].should_not be_nil
+    end
+
+    it "should not create bad ones" do
+      any_instance_of(Invite, :valid? => false)
+
+      post 'create'
+      response.should be_redirect
+      flash[:alert].should_not be_nil
+    end
+  end
+
+  describe "#destroy" do
+    it "should destroy" do
+      stub(Invite).find('37') { mock(Invite.new).destroy }
+      
+      post 'destroy', :id => 37
+      response.should be_redirect
+      flash[:notice].should_not be_nil
+    end
   end
 end
