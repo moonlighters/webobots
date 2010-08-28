35dc685183d25bdb4c6d5b1aa156ac024ff2ffa8
NIA <nia.fti@mail.ru>
2010-08-28 19:43:29 +0700

    Спеки на MatchesController

--- spec/controllers/matches_controller_spec.rb
+++ spec/controllers/matches_controller_spec.rb
@@ -1,4 +1,108 @@
 require 'spec_helper'
 
 describe MatchesController do
+  include AuthlogicSpecHelpers
+  before { login }
+
+  %w{index all}.each do |action|
+    describe "##{action}" do
+      it "should list items" do
+        get action
+        response.should be_success
+      end
+    end
+  end
+
+  describe "#all_for_user" do
+    it "should list items" do
+      mock(User).find('37') { Factory :user }
+
+      get 'all_for_user', :user_id => 37
+      response.should be_success
+    end
+  end
+
+  describe "#all_for_firmware" do
+    it "should list items" do
+      fw = Factory :firmware
+      stub(Firmware).find.with_any_args { |*args| fw }
+
+      get 'all_for_firmware', :firmware_id => 37
+      response.should be_success
+    end
+  end
+
+  describe "#new" do
+    it "should work if user has firmwares" do
+      stub(current_user).firmwares { stub(Firmware).count { 2 }.subject }
+
+      get 'new'
+      response.should be_success
+    end
+
+    it "should notify user if he has no firmwares" do
+      stub(current_user).firmwares { stub(Firmware).count { 0 }.subject }
+
+      get 'new'
+      response.should be_redirect
+      flash[:alert].should_not be_nil
+    end
+
+    it "should not work when not logged it" do
+      logout
+
+      get 'new'
+      response.should be_redirect
+      flash[:alert].should_not be_nil
+    end
+  end
+
+  describe "#create" do
+    it "should create good match" do
+      any_instance_of Match, :valid? => true
+      any_instance_of Match, :emulate => :draw
+
+      post 'create'
+      response.should be_redirect
+    end
+
+    it "should not create bad match" do
+      any_instance_of Match, :valid? => false
+
+      post 'create'
+      response.should render_template 'new'
+    end
+  end
+
+  describe "#show" do
+    it "should work" do
+      m = Factory :match
+      mock(Match).find('37') { m }
+      
+      get 'show', :id => 37
+      response.should be_success
+    end
+  end
+
+  describe "#play" do
+    before do
+      m = Factory :match
+      mock(Match).find('37') { m }
+      stub(m).emulate { :draw }
+    end
+
+    it "should play match if request.xhr?" do
+      stub(request).xhr? { true }
+
+      get 'play', :id => 37
+      response.should be_success
+    end
+
+    it "should redirect to match path unless request.xhr?" do
+      stub(request).xhr? { false }
+
+      get 'play', :id => 37
+      response.should be_redirect
+    end
+  end
 end
