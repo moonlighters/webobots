0e962c450605dd42d521665b8b9200f383b22011
NIA <nia.fti@mail.ru>
2010-08-29 18:28:37 +0700

    Спеки на UsersController

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,10 +1,76 @@
 require 'spec_helper'
 
 describe UsersController do
+  include AuthlogicSpecHelpers
+  integrate_views
 
-  #Delete this example and add some real ones
-  it "should use UsersController" do
-    controller.should be_an_instance_of(UsersController)
+  before { login }
+
+  describe "#index" do
+    it "should work" do
+      get 'index'
+      response.should be_success
+    end
   end
 
+  describe "#new" do
+    it "should work" do
+      logout
+      
+      get 'new'
+      response.should be_success
+    end
+  end
+
+  describe "#create" do
+    before { logout }
+
+    it "should register valid user" do
+      any_instance_of User, :valid? => true
+      # mock на save!, а не только на valid?, потому что без вызова
+      # before_validation сохранение юзера падает (Authlogic)
+      any_instance_of User, :save! => true
+      
+      # код '1234': используем глобальный хак для инвайтов...
+      post 'create', :user => {:code => '1234'}
+      response.should be_redirect
+      flash[:notice].should_not be_nil
+    end
+
+    it "should not register invalid user" do
+      any_instance_of User, :valid? => false
+      
+      post 'create', :user => {}
+      response.should render_template 'new'
+    end
+  end
+
+  %w{show edit}.each do |action|
+    describe "##{action}" do
+      it "should work" do
+        u = Factory :user
+        mock(User).find('37') { u }
+
+        get 'show', :id => 37
+        response.should be_success
+      end
+    end
+  end
+
+  describe "#update" do |action|
+    it "should apply good update" do
+      mock(current_user).valid?.times(any_times) { true }
+
+      put 'update', :user => {}
+      response.should be_redirect
+      flash[:notice].should_not be_nil
+    end
+
+    it "should not apply bad update" do
+      mock(current_user).valid?.times(any_times) { false }
+
+      put 'update', :user => {}
+      response.should render_template 'edit'
+    end
+  end
 end
