43e910301d64a17adb50573c1570de6bcd3baa7e
Vladimir Parfinenko <vladimir.parfinenko@gmail.com>
2010-09-21 23:39:04 +0700

    Крутой хелпер для плюрализации

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,17 @@ module ApplicationHelper
   def format_datetime(time)
     Russian.strftime time, "%e %B %Y, %H:%M"
   end
+
+  # Возвращает строку полученную преобразованием строки +format+ по следующим правилам:
+  # * '%?' заменяется на число +number+
+  # * '%{one,few,many,other}' заменяется на один из вариантов в соответствии с правилами руссками языка
+  def pl(format, number)
+    format.tap do |s|
+      s.gsub! '%?', number.to_s
+      s.gsub! /%\{[^}]+\}/ do |sub|
+        variants = sub[2...-1].split(',', -1)
+        Russian::pluralize number, *variants
+      end
+    end
+  end
 end
