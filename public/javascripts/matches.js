70b698f6d00043f635acb49700925ce97832145e
Vladimir Parfinenko <vladimir.parfinenko@gmail.com>
2010-10-16 01:48:39 +0700

    Страничка просмотра списка матчей

--- public/javascripts/matches.js
+++ public/javascripts/matches.js
@@ -1,6 +1,7 @@
 $(function() {
   $('.match_entity .more_link').click(function (e){
-    $(this).closest('.match_entity').find('.more').slideToggle('fast');
+    $(this).closest('.match_entity').toggleClass('expanded')
+    .find('.more').slideToggle('fast');
     e.preventDefault();
   });
 });
